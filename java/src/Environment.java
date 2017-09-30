50017ed7787d09c3886ab851abe90d402de3a24b
kverma251995 <31871652+kverma251995@users.noreply.github.com>
2017-09-30 11:12:26 -0700

    Update Environment.java

--- java/src/Environment.java
+++ java/src/Environment.java
@@ -5,7 +5,7 @@ public class Environment {
     public List<Car> cars;
 
     public Environment() {
-        // this.cars = new ArrayList<>();
+         this.cars = new ArrayList<>();
     }
 
     public double get_next_station_following(double car_position){
