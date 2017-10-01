71b8aaa77a9cd60bbedb224aab349384ca70d8ff
Qichang Hu <31938686+YujeuHu@users.noreply.github.com>
2017-10-01 16:00:46 -0700

    HW1 Matlab Commitment

--- matlab/Environment.m
+++ matlab/Environment.m
@@ -2,12 +2,19 @@ classdef Environment < handle
     
     properties
         cars
+      
     end
     
     methods
         
         function this = Environment()
             this.cars = repmat(Car,1,0);
+    
+        end
+        
+        function [w] = add_car(this, cars)
+            w=0;
+            
         end
         
         function [x] = get_next_station_following(this, car_position)
