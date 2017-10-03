0651ce6c60667607d292b9de60afdf5180453db7
zsc1123 <32053749+zsc1123@users.noreply.github.com>
2017-10-02 20:41:01 -0700

    hw2_matlab

--- matlab/Environment.m
+++ matlab/Environment.m
@@ -9,7 +9,10 @@ classdef Environment < handle
         function this = Environment()
             this.cars = repmat(Car,1,0);
         end
-        
+        function this=add_car(this,carn)
+            n=size(this.cars,2);
+            this.cars(n+1)=carn;
+        end
         function [x] = get_next_station_following(this, car_position)
         	x = inf;
         end
