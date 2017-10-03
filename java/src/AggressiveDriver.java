27751981aeebf09ef9478d519170b8cb6dc0d93f
sikang hu <sikang_hu@berkeley.edu>
2017-10-03 16:50:43 -0700

    submit java file

--- java/src/AggressiveDriver.java
+++ java/src/AggressiveDriver.java
@@ -1,7 +1,14 @@
+import static jdk.nashorn.internal.objects.Global.Infinity;
+
 public class AggressiveDriver extends Driver {
 
     public AggressiveDriver(String name, double weight) {
         super(name, weight);
+    }
+
+    @Override
+    public double throttle_action(Car car, Environment env) {
+        return Infinity;
     }
 
     @Override
