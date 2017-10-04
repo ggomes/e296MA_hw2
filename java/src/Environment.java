bb0bd332a7c5a2c45453d1cd1741340f34fd8b9e
MoyuLi <32108522+MoyuLi@users.noreply.github.com>
2017-10-04 01:11:07 -0700

    Correct code

--- java/src/Environment.java
+++ java/src/Environment.java
@@ -1,11 +1,16 @@
-import java.util.List;
+import java.util.*;
 
 public class Environment {
 
     public List<Car> cars;
 
     public Environment() {
-        // this.cars = new ArrayList<>();
+         this.cars = new ArrayList<Car>();
+    }
+
+    public void add(Car cars){
+        this.cars.add(cars);
+
     }
 
     public double get_next_station_following(double car_position){
